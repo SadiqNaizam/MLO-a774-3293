d413db526917afdf7cbcd67d4a024fa0ba256066
SadiqNaizam <sadiq.naizam1234@gmail.com>
2025-06-18 12:04:45 +0530

    Updated code files: LoginPage.tsx

--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -59,7 +59,7 @@ const LoginPage: React.FC = () => {
   };
 
   return (
-    <div className="flex flex-col min-h-screen bg-gray-50 dark:bg-gray-900">
+    <div className="flex flex-col min-h-screen bg-background text-foreground">
       <Header />
       <main className="flex-1 flex flex-col items-center justify-center p-4 sm:p-6 lg:p-8">
         <AuthFormCard
